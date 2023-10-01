5ee152f596c2344b07d4c4d86e6eec0e2fb694ed
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2023-10-01 15:08:50 +0200

    left turning behavour

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour
 {
     public float forwardSpeed = 5.0f;
-    public float rightCorneringSpeed = 3.0f;
-    public float rightTurningSpeed = 3.0f;
+    public float corneringSpeed = 3.0f;
+    public float rightTurningSpeed = 0.4f;
+    public float leftTurningSpeed = 0.2f;
     public GameObject actionTrigger;
 
-    private char action = 'a'; // a: arrive | f: forward | l: left | r: right | e: exit
+    private char action = 'f'; // f: forward | l: left | r: right
     private Quaternion targetRotation;
     private Vector3 direction;
     private Vector3 carPosition;
@@ -17,9 +18,12 @@ public class CarMovement : MonoBehaviour
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other) {
-        if(other.gameObject == actionTrigger) {
+        if(other.name == "Forward-Right-TriggerBox") {
             targetRotation = Quaternion.Euler(0, transform.eulerAngles.y + 90.0f, 0);
             action = 'r';
+        } else if(other.name == "Left-TriggerBox") {
+            targetRotation = Quaternion.Euler(0, transform.eulerAngles.y -90.0f, 0);
+            action = 'l';
         }
     } 
 
@@ -28,9 +32,19 @@ public class CarMovement : MonoBehaviour
         carPosition = transform.position;
 
         if(action == 'r') {
-            transform.Translate(direction * rightCorneringSpeed * Time.deltaTime, Space.World);        
+            transform.Translate(direction * corneringSpeed * Time.deltaTime, Space.World);        
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rightTurningSpeed);
-        } else {
+            if(transform.rotation == targetRotation) {
+                action = 'f';
+            }
+        } else if (action == 'l') {
+            transform.Translate(direction * corneringSpeed * Time.deltaTime, Space.World);        
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, leftTurningSpeed);
+            if(transform.rotation == targetRotation) {
+                action = 'f';
+            }
+    
+        } else{
             transform.Translate(direction * forwardSpeed * Time.deltaTime, Space.World);        
         }
     }
