2bda64b7303de40044827b10620296c8206db052
EliottDup <45095693+EliottDup@users.noreply.github.com>
2023-11-29 12:23:02 +0100

    updated some things inside the simutation runner

--- Assets/Scripts/NewSim/Simulation.cs
+++ Assets/Scripts/NewSim/Simulation.cs
@@ -8,19 +8,22 @@ public class Simulation : MonoBehaviour
     //DIT WORDT DE INTERFACE MET DE SIMULATOR, SIMULATOR IS GEWOON EEN AANTAL FUNCTIES, DIT GAAT DEZE FUNCTIES AANROEPEN EN NAAR DE AI STUREN
 
     public float timeStepSize = 0.05f;
-    public float timeBetweenSteps = 0.05f;
+    public float timeBetweenVisualisations = 0.05f;
 
     public int seed;
     public Visualizer visualiser;
     Simulator.Simulator simulator = new Simulator.Simulator();
+
+    public bool runAtSetSpeed = true;
+    public bool paused = false;
 
     void Start()
     {
         simulator.seed = seed;
         simulator.TestPopulation();
         Visualize();
-
         Step();
+        VisualsUpdater();
     }
 
     void Visualize()
@@ -30,9 +33,21 @@ public class Simulation : MonoBehaviour
 
     void Step()
     {
-        simulator.Step(timeStepSize);
-        Invoke(nameof(Step), timeBetweenSteps);
+        if (!paused)
+            simulator.Step(timeStepSize);
+        if (runAtSetSpeed)
+        {
+            Invoke(nameof(Step), timeStepSize);
+            return;
+        }
+        Invoke(nameof(Step), 0);
+
+    }
+
+    void VisualsUpdater()
+    {
         Visualize();
+        Invoke(nameof(VisualsUpdater), timeBetweenVisualisations);
     }
 
 }
