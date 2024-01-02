8aea4dfd8b426bb795b9ebeaf31ccaeef01a538a
EliottDup <45095693+EliottDup@users.noreply.github.com>
2024-01-02 11:48:18 +0100

    started minimal work on the trafficLightControllerNL

--- Assets/Scripts/New Simulation/SimulationAPI/TrafficLightControllerNL.cs
+++ Assets/Scripts/New Simulation/SimulationAPI/TrafficLightControllerNL.cs
@@ -1,7 +1,16 @@
-namespace SimulationAPI{
-    
-    class TrafficControlSystemNL{
-        
+using System.Collections.Generic;
+
+namespace SimulationAPI
+{
+
+    class TrafficControlSystemNL
+    {
+        Dictionary<string, Dictionary<string, int>> inTable;
+
+        void Update(ref List<Light> lights)
+        {
+
+        }
     }
 
 }
