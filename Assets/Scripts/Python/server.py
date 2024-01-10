85997e758ef485d8c981f9f16ed782bbec631ad9
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2024-01-10 16:01:17 +0100

    pytorch working

--- Assets/Scripts/Python/server.py
+++ Assets/Scripts/Python/server.py
@@ -1,6 +1,35 @@
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 
+import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+env = gym.make("CartPole-v1")
+
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+# if GPU is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+x = torch.rand(5, 3)
+print(x)
+
+
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -16,6 +45,9 @@ class MyHandler(SimpleHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(b'Received POST data: ' + post_data)
+
+
+
 
 if __name__ == "__main__":
     PORT = 8000
