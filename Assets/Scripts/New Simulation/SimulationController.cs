e24b8de82e12655ecc6500d1ac5a5e94ad8c5904
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2024-01-17 15:46:18 +0100

    give done veriable to python deepQnet API

--- Assets/Scripts/New Simulation/SimulationController.cs
+++ Assets/Scripts/New Simulation/SimulationController.cs
@@ -17,7 +17,7 @@ public class ResponseData {
 public class SimulationController : MonoBehaviour
 {
     //DIT WORDT DE INTERFACE MET DE SIMULATOR, SIMULATOR IS GEWOON EEN AANTAL FUNCTIES, DIT GAAT DEZE FUNCTIES AANROEPEN EN NAAR DE AI STUREN
-
+    public int stepCount;
     public float timeStepSize = 0.05f;
     public float timeBetweenVisualisations = 0.05f;
 
@@ -35,14 +35,14 @@ public class SimulationController : MonoBehaviour
     private QLearnAgent qAgent;
     private bool isAIControlled;
     private int[] networkNeuronCounts = { 4, 6, 4 }; // [3*(4*carLimit), ~, cycles]
-    private int maxIterations;
+    private int maxIterations = 5000;
 
     private int currentAction;
+    private string SERVER_URL = "http://localhost:8000/";
 
     void Start()
     {
         currentAction = -2;
-        maxIterations = 50_000; // TO DO: maxIterations given as parameter of Start()  //
         isAIControlled = true; // TO DO: isAIControlled given as parameter of Start() //
         if (isAIControlled)
         {
@@ -54,14 +54,18 @@ public class SimulationController : MonoBehaviour
         Step();
         VisualsUpdater();
         lastframe = Time.time;
-        StartCoroutine(GetRequest("http://localhost:8000/"));
-        // int action = JsonConvert.DeserializeObject<Action>(json).action;
-        StartCoroutine(Upload());
     }
 
-    IEnumerator Upload()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8000/", "{ \"state\": 1, \"action\": 2, \"reward\": 2, \"nextState\": 2, \"done\": 1 }", "application/json"))
+    IEnumerator Upload(int state, int action, int reward, int nextState, int done)
+    {   
+        string data =  
+            "{ \"state\": " + state 
+            + ", \"action\": " + action 
+            + ", \"reward\": " + reward 
+            + " , \"nextState\": " + nextState 
+            + ", \"done\": " + done + " }";
+        
+        using (UnityWebRequest www = UnityWebRequest.Post(this.SERVER_URL, data, "application/json"))
         {
             yield return www.SendWebRequest();
 
@@ -76,14 +80,14 @@ public class SimulationController : MonoBehaviour
         }
     }
 
-    IEnumerator GetRequest(string uri)
+    IEnumerator GetRequest()
     {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(this.SERVER_URL))
         {
             // Request and wait for the desired page.
             yield return webRequest.SendWebRequest();
 
-            string[] pages = uri.Split('/');
+            string[] pages = this.SERVER_URL.Split('/');
             int page = pages.Length - 1;
 
             switch (webRequest.result)
@@ -118,29 +122,26 @@ public class SimulationController : MonoBehaviour
 
     void Step()
     {
+        this.stepCount++;
         if (!paused)
         {
-            if (isAIControlled)
+            if (isAIControlled && this.stepCount % 20 == 0)
             {
-                // NOTE: qAgent stepSize could be bigger than simulator stap in future //
-                List<float> debugValues = qAgent.Step(simulator);
+                int done = this.stepCount / this.maxIterations;
+                StartCoroutine(GetRequest());
+                StartCoroutine(Upload(2, this.currentAction, 3, 3, done));
+                // List<float> debugValues = qAgent.Step(simulator);
 
-                // for(int i = 0; i < debugValues.Count; i++) {
-                //     if(i % 24 == 0){
-                //         print("NEXXXXT LINE _____________________");
-                //     }
-                //     print("ln.70: " + debugValues[i] + " value " + i);
-                // }
-                isAIControlled = false;
+
             }
-            simulator.Step(timeStepSize);
+            simulator.Step(this.timeStepSize);
             float dt = Time.time - lastframe;
             lastframe = Time.time;
             updateTPSCounter(dt);
         }
         if (runAtSetSpeed)
         {
-            Invoke(nameof(Step), timeStepSize);
+            Invoke(nameof(Step), this.timeStepSize);
         }
         else
         {
@@ -150,7 +151,7 @@ public class SimulationController : MonoBehaviour
 
     void updateTPSCounter(float dt)
     {
-        tpsCounter.text = "tps:  " + Mathf.Clamp(Mathf.Round(1 / dt), 0, runAtSetSpeed ? 1 / timeStepSize : 99999).ToString();
+        tpsCounter.text = "tps:  " + Mathf.Clamp(Mathf.Round(1 / dt), 0, this.runAtSetSpeed ? 1 / this.timeStepSize : 99999).ToString();
         dt = Mathf.Round(dt * 10f) / 10f;
         MSPTCounter.text = "mspt: " + (dt * 1000).ToString();
     }
